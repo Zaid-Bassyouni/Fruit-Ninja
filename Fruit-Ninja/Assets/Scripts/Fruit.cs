59e530b9faf81c225722075a5a4466b6d85e336e
Zaid Bassyouni <zaidbassyouni@gmail.com>
2023-10-14 18:32:40 +0300

    Slicing is Done!

---
+++ Fruit-Ninja/Assets/Scripts/Fruit.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject Whole;
+    public GameObject Sliced;
+
+    private Rigidbody fruitRigidbody;
+    private Collider fruitCollider;
+
+
+    private void Awake()
+    {
+        fruitCollider = GetComponent<Collider>();
+        fruitRigidbody = GetComponent<Rigidbody>();
+    }
+    private void Slice(Vector3 direction , Vector3 position , float force)
+    {
+        Whole.SetActive(false);
+        Sliced.SetActive(true);
+
+        fruitCollider.enabled = false;
+
+        float angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg ;
+        Sliced.transform.rotation = Quaternion.Euler(0f,0f,angle);
+
+        Rigidbody[] slices = Sliced.GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody slice in slices) 
+        {
+            slice.velocity = fruitRigidbody.velocity;
+            slice.AddForceAtPosition(direction*force ,position,ForceMode.Impulse);
+
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Blade blade = other.GetComponent<Blade>();
+            Slice(blade.direction, blade.transform.position,blade.SliceForce);
+        }
+    }
+
+}
